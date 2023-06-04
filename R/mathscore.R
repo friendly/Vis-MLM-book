fab0065562165aa09614062030fe0899d21e3819
Michael Friendly <friendly@yorku.ca>
2023-06-04 16:12:48 -0400

    more work on hotelling.qmd

--- R/mathscore.R
+++ R/mathscore.R
@@ -1,6 +1,6 @@
 library(car)
 library(heplots)
-library(DescTools)
+#library(DescTools)
 library(dplyr)
 
 
@@ -22,7 +22,19 @@ mathscore |>
 # two-sample test
 DescTools::HotellingsT2Test(cbind(BM, WP) ~ group, data=mathscore)
 
-mod <- lm(cbind(BM, WP) ~ group, data=mathscore)
+library(Hotelling)
+ht <- hotelling.test(cbind(BM, WP) ~ group, data=mathscore)
+str(ht)
+
+T2 <- ht$stats$statistic
+N <- nrow(mathscore)
+p <- 2
+(F <- ((N-p)/(p * (N-1))) * T2)
+
+
+
+
+math.mod <- lm(cbind(BM, WP) ~ group, data=mathscore)
 car::Anova(mod)
 
 summary(Anova(mod))
@@ -33,24 +45,14 @@ print(summary(Anova(mod)), SSP=FALSE)
 means <-  aggregate(cbind(BM, WP)~group, mean, data=mathscore)[,-1]
 
 
-
-# covEllipses(mathscore[,2:3], mathscore$group, 
-#             pooled=FALSE, 
-#             fill = TRUE, fill.alpha = 0.05,
-#             cex=2, cex.lab=1.5,
-#             xlab="Basic math", ylab="Word problems")
-# 
-# pch <- ifelse(mathscore$group==1, 15, 16)
-# col <- ifelse(mathscore$group==1, "red", "blue")
-# points(mathscore[,2:3], pch=pch, col=col, cex=1.25)
-
 # use formula method for covEllipses
 
 colors <- c("darkgreen", "blue")
 covEllipses(cbind(BM, WP) ~ group, data = mathscore,
             pooled=FALSE, 
             col = colors,
-            fill = TRUE, fill.alpha = 0.05,
+            fill = TRUE, 
+            fill.alpha = 0.05,
             cex=2, cex.lab=1.5,
             asp = 1,
             xlab="Basic math", ylab="Word problems")
@@ -59,22 +61,33 @@ pch <- ifelse(mathscore$group==1, 15, 16)
 col <- ifelse(mathscore$group==1, colors[1], colors[2])
 points(mathscore[,2:3], pch=pch, col=col, cex=1.25)
 
+# show S_p
+covEllipses(cbind(BM, WP) ~ group, data = mathscore,
+            col = c(colors, "red"),
+            fill = c(FALSE, FALSE, TRUE), 
+            fill.alpha = 0.3,
+            cex=2, cex.lab=1.5,
+            asp = 1,
+            xlab="Basic math", ylab="Word problems")
 
 
-car::scatterplot(WP ~ BM | group, data=mathscore, 
-	ellipse=list(levels=0.68), 
-	smooth=FALSE, 
-	pch=c(15,16), 
-	asp = 1, 
-	cex.lab = 1.5,
-	legend=list(coords = "topright"),
-	xlab="Basic math", ylab="Word problems"
-)
+# car::scatterplot(WP ~ BM | group, data=mathscore, 
+# 	ellipse=list(levels=0.68), 
+# 	smooth=FALSE, 
+# 	pch=c(15,16), 
+# 	asp = 1, 
+# 	cex.lab = 1.5,
+# 	legend=list(coords = "topright"),
+# 	xlab="Basic math", ylab="Word problems"
+# )
 
 
 op <- par(mar=c(4,5,1,1)+.2)
-heplot(mod, fill=TRUE, cex=2, cex.lab=1.8,
-	xlab="Basic math", ylab="Word problems")
+heplot(mod, 
+       fill=TRUE, lwd = 3,
+       asp = 1,
+       cex=2, cex.lab=1.8,
+       xlab="Basic math", ylab="Word problems")
 par(op)
 
 library(candisc)
@@ -88,18 +101,24 @@ plot(mod.can, var.lwd=3)
 
 # overlay with HEplot
 covEllipses(cbind(BM, WP) ~ group, data = mathscore,
-            pooled=FALSE, cex=2, 
-            xlab="Basic math", ylab="Word problems",
+            pooled=FALSE, 
+            col = colors,
+            cex=2, cex.lab=1.5,
             asp=1, 
-            cex.lab=1.5)
-
+            xlab="Basic math", ylab="Word problems"
+            )
 pch <- ifelse(mathscore$group==1, 15, 16)
 col <- ifelse(mathscore$group==1, "red", "blue")
 points(mathscore[,2:3], pch=pch, col=col, cex=1.25)
 
-heplot(mod, fill=TRUE, cex=2, cex.lab=1.8, 
-	col=c("red", "black"), fill.alpha=0.2, lwd=c(1,3),
-	xlab="Basic math", ylab="Word problems", add=TRUE, error.ellipse=TRUE)
+math.mod <- lm(cbind(BM, WP) ~ group, data=mathscore)
+heplot(math.mod, 
+       fill=TRUE, 
+       cex=2, cex.lab=1.8, 
+  	   fill.alpha=0.2, lwd=c(1,3),
+	     add = TRUE, 
+       error.ellipse=TRUE)
+
 
 
 # find projection of point A on line between L1 and L2
@@ -113,10 +132,15 @@ project_on <- function(A, L1, L2) {
 	C
 }
 
+# find group means
+means <- mathscore |>
+  group_by(group) |>
+  summarize(BM = mean(BM), WP = mean(WP))
+
 # why are these not orthogonal to the line???
 for(i in 1:nrow(mathscore)) {
 	gp <- mathscore$group[i]
-	pt <- project_on( mathscore[i, 2:3], means[1,], means[2,]) 
+	pt <- project_on( mathscore[i, 2:3], means[1,2:3], means[2,2:3]) 
 #	print(pt)
 	segments(mathscore[i, "BM"], mathscore[i, "WP"], pt[1], pt[2])
 }
