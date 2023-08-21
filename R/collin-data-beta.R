7b58ee3b8e61ac25c5fb9d21c860e9a867145ef3
Michael Friendly <friendly@yorku.ca>
2023-08-21 15:46:34 -0400

    work on collinearity-ridge.qmd

--- R/collin-data-beta.R
+++ R/collin-data-beta.R
@@ -4,98 +4,77 @@
 #' 
 library(MASS)
 library(car)
-library(dplyr)
-library(tibble)
+# library(dplyr)
+# library(tibble)
 
-set.seed(42)
-N <- 100
-mu <- c(0, 0)
-s <- c(1, 1)
-rho <- c(0, 0.6, 0.97)
+set.seed(421)
+N <- 200
+mu <- c(0, 0)            # means
+s <- c(1, 1)             # standard deviations
+rho <- c(0, 0.8, 0.97)   # correlations
+beta <- c(3, 3)          # true coefficients
 
 # covariance matrix, with standard deviations s[1], s[2] and correlation r
 Cov <- function(s, r){
-  matrix(c(s[1], r * prod(s),
+  matrix(c(s[1],    r * prod(s),
          r * prod(s), s[2]), nrow = 2, ncol = 2)
 }
 
-do_plots <- function(X, y, r) {
-  dataEllipse(X,
-              levels= 0.95,
-              fill = TRUE, fill.alpha = 0.05,
-              xlim = c(-3, 3),
-              ylim = c(-3, 3), asp = 1)
-  text(-3, -3, expression(paste(rho, " = ", r )))
-  mod <- lm(y ~ X[,1] + X[,2])
-  
-  confidenceEllipse(mod,
-                    col = "pink",
-                    fill = TRUE, fill.alpha = 0.1,
-                    xlab = "x1 coefficient",
-                    ylab = "x2 coefficient",
-                    asp = 1)
-  abline(v=0, h=0, lwd=2)
-}
-
-X <- mvrnorm(N, mu, Sigma = Cov(s, rho[1]))
-colnames(X) <- c("x1", "x2")
-y <- 2 * X[,1] + 2 * X[,2] + rnorm(N, 0, 10)
-mod <- lm(y ~ X[,1] + X[,2])
-
-dataEllipse(X,
-            levels= 0.95,
-            col = "darkgreen",
-            fill = TRUE, fill.alpha = 0.05,
-            xlim = c(-3, 3),
-            ylim = c(-3, 3), asp = 1)
-text(-3, 2.5, bquote(rho == .(rho[1])), cex = 2, pos = 4)
-
-confidenceEllipse(mod,
-                  col = "pink",
-                  fill = TRUE, fill.alpha = 0.1,
-                  xlab = "x1 coefficient",
-                  ylab = "x2 coefficient",
-                  xlim = c(-5, 10),
-                  ylim = c(-5, 10),
-                  asp = 1)
-abline(v=0, h=0, lwd=2)
-
-do_plots(X, y, rho[1])
-
-X <- mvrnorm(N, mu, Sigma = Cov(s, rho[2]))
-colnames(X) <- c("x1", "x2")
-y <- 2 * X[,1] + 2 * X[,2] + rnorm(N, 0, 10)
-do_plots(X, y)
-
-X <- mvrnorm(N, mu, Sigma = Cov(s, rho[3]))
-colnames(X) <- c("x1", "x2")
-y <- 2 * X[,1] + 2 * X[,2] + rnorm(N, 0, 10)
-do_plots(X, y)
-
-
-
-op <- par(mar = c(4,4,1,1)+0.1,
-          mfcol = c(3, 2))
-for (r in seq_along(rho)) {
+# Generate a dataframe of X, y for each rho
+# Fit the model for each
+XY <- vector(mode ="list", length = length(rho))
+mods <- vector(mode ="list", length = length(rho))
+for (i in seq_along(rho)) {
+  r <- rho[i]
   X <- mvrnorm(N, mu, Sigma = Cov(s, r))
   colnames(X) <- c("x1", "x2")
-  y <- 2 * X[,1] + 2 * X[,2] + rnorm(N, 0, 10)
-  
+  y <- beta[1] * X[,1] + beta[2] * X[,2] + rnorm(N, 0, 10)
+
+  XY[[i]] <- data.frame(X, y=y)
+  mods[[i]] <- lm(y ~ x1 + x2, data=XY[[i]])
+}
+
+# coefficients
+coefs <- sapply(mods, coef)
+colnames(coefs) <- c("Intercept", "b1", "b2")
+coefs
+
+# VIFs
+vifs <- sapply(mods, car::vif)
+colnames(vifs) <- paste("rho:", rho)
+vifs
+
+do_plots <- function(XY, mod, r) {
+  X <- as.matrix(XY[, 1:2])
   dataEllipse(X,
               levels= 0.95,
+              col = "darkgreen",
               fill = TRUE, fill.alpha = 0.05,
               xlim = c(-3, 3),
               ylim = c(-3, 3), asp = 1)
-  
-  mod <- lm(y ~ X[,1] + X[,2])
-  
+  text(0, 3, bquote(rho == .(r)), cex = 2, pos = NULL)
+
   confidenceEllipse(mod,
+                    col = "red",
                     fill = TRUE, fill.alpha = 0.1,
                     xlab = "x1 coefficient",
                     ylab = "x2 coefficient",
+                    xlim = c(-5, 10),
+                    ylim = c(-5, 10),
                     asp = 1)
+  points(beta[1], beta[2], pch = "+", cex=2)
   abline(v=0, h=0, lwd=2)
-  
 }
 
+op <- par(mar = c(4,4,1,1)+0.1,
+          mfcol = c(2, 3),
+          cex.lab = 1.5)
+
+for (i in seq_along(rho)) {
+  do_plots(XY[[i]], mods[[i]], rho[i])
+}
 par(op)
+
+
+
+
