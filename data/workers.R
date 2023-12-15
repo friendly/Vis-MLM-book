29f42cba2bdcee9e1109094faee7863e58bfd33d
Michael Friendly <friendly@yorku.ca>
2023-12-15 17:47:17 -0500

    add multivariate-multivariable; add workers$Sat

--- data/workers.R
+++ data/workers.R
@@ -1,5 +1,26 @@
 # workers data
 
+library(dplyr)
+library(heplots)
 workers <- read.csv(here::here("data", "workers.csv"))
+
+workers.mod <- lm(Income ~ Experience + Skill + Gender, data=workers)
+summary(workers.mod)
+coef(workers.mod)
+
+set.seed(42)
+# create job satisfaction measure
+workers <- workers |> 
+  mutate(
+    Sat = Income/5 - 
+          Experience / 2 + 
+          Skill/2 + (Gender=="Female"),
+    Sat = Sat + rnorm(n = nrow(workers), mean=0, sd = 1) |> round())
+  relocate(Sat, .after = Income)
+
+workers.mlm <- lm(cbind(Income, Sat) ~ Experience + Skill + Gender, data=workers)
+summary(workers.mlm)
+
+heplot(workers.mlm)
 
 save(workers, file = here::here("data", "workers.RData"))
