483a00c9dd5b6ecfeafef446342cf107c55a5a04
Michael Friendly <friendly@yorku.ca>
2024-05-10 22:04:34 -0400

    add CIs to coffee data ellipse

--- R/coffee/coffee-data-beta.R
+++ R/coffee/coffee-data-beta.R
@@ -32,25 +32,32 @@ dataEllipse(Stress ~ Coffee, data = coffee,
             levels = 0.68,
             ellipse.label = 0.68,
             center.cex = 2, cex.lab = 1.5,
-            fill = FALSE)
+            fill = FALSE,
+            xlim = c(0, 160),
+            ylim = c(0, 200))
 dataEllipse(Stress ~ Coffee, data = coffee,
             levels = 0.40,
             ellipse.label = 0.40,
             fill = TRUE, fill.alpha = 0.1,
             col = "red", add = TRUE, plot.points = FALSE)
 
-coffee |> tidyr::pivot_longer(Coffee:Stress, names_to = "variable") |>
-  select(-Group, -Heart) 
-
-coffee |>
+stats <- coffee |> 
+  tidyr::pivot_longer(Coffee:Stress, names_to = "variable") |>
   select(-Group, -Heart) |>
-  summarise(across(Coffee:Stress, .fns = c(mean, sd)))
-#  summarise_each(funs(mean,sd,se=sd(.)/sqrt(n())))  
-
-
+  group_by(variable) |>
+  summarise(mean = mean(value), sd = sd(value), 
+#            se = sd/sqrt(n()),
+            lower = mean - sd,
+            upper = mean + sd
+            ) 
+CI <- stats[, c("lower", "upper")]
+lines( y = c(10,10), x = CI[1,] , lwd = 6, col = 'red')
+lines( x = c( 0, 0), y = CI[2,] , lwd = 6, col = 'red')
+points(y = 10, x = stats[1, "mean"], col = 'black', pch = 16, cex=1.8)
+points(x =  0, y = stats[2, "mean"], col = 'black', pch = 16, cex=1.8)
 
 abline(lm(Stress ~ Coffee, data = coffee), lwd = 2)
-text(21, 180, "Data space", cex = 2, pos = 4)
+text(0, 180, "Data space", cex = 2, pos = 4)
 par(op)
 
 
