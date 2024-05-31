ac36e8b2abf2b406e9dd835e9d64b4ff065d9cee
Michael Friendly <friendly@yorku.ca>
2024-05-31 17:40:49 -0400

    coefplots

--- R/prestige/prestige-coefplot.R
+++ R/prestige/prestige-coefplot.R
@@ -14,24 +14,46 @@ library(modelsummary)
 library(GGally)
 library(broom.helpers)
 
+data(Prestige, package = "carData")
 
 #' plots of unstandardized coefficients
 
-mod0 <- lm(prestige ~ education + income + women,
+mod1 <- lm(prestige ~ education + income + women,
            data=Prestige)
 
-mod1 <- lm(prestige ~ education + women + income + type,
+# display coefficients
+lmtest::coeftest(mod1)
+
+# or use model summary
+modelsummary(list("Model1" = mod1),
+  coef_omit = "Intercept",
+#  output = "markdown",
+  stars = TRUE,
+  shape = term ~ statistic,
+  statistic = c("std.error", "p.value"),
+  gof_omit = ".")
+
+
+# plot the coefficients
+
+theme_set(theme_minimal(base_size = 14))
+
+modelplot(mod1, coef_omit="Intercept", 
+          color="red", size=1) +
+  labs(title="Raw coefficients for mod1")
+
+# fit other models
+mod2 <- lm(prestige ~ education + women + income + type,
            data=Prestige)
 
-mod2 <- lm(prestige ~ education + women + income * type,
+mod3 <- lm(prestige ~ education + women + income * type,
                    data=Prestige)
 
-modelplot(list("mod0" = mod0, "mod1" = mod1, "mod2" = mod2), 
-          coef_omit="Intercept", size=1.3) +
+modelplot(list("mod1" = mod1, "mod2" = mod2, "mod3" = mod3), 
+          coef_omit="Intercept", size=1.3, linewidth=2) +
   ggtitle("Raw coefficients") +
-  geom_vline(xintercept = 0, size=1.5) +
+  geom_vline(xintercept = 0, linewidth=1.5) +
   scale_y_discrete(limits=rev) +
-  theme_bw(base_size = 16) +
   theme(legend.position = "inside",
         legend.position.inside = c(0.85, 0.2))
 
@@ -41,20 +63,32 @@ Prestige_std <- Prestige %>%
   as_tibble() %>%
   mutate(across(where(is.numeric), scale))
 
-mod0_std <- lm(prestige ~ education + income + women,
+mod1_std <- lm(prestige ~ education + income + women,
               data=Prestige_std)
 
-mod1_std <- lm(prestige ~ education + women + income + type,
+mod2_std <- lm(prestige ~ education + women + income + type,
                data=Prestige_std)
 
-mod2_std <- lm(prestige ~ education + women + income * type,
+mod3_std <- lm(prestige ~ education + women + income * type,
                data=Prestige_std)
 
-models <- list("mod0" = mod0_std, "mod1" = mod1_std, "mod2" = mod2_std)
+models <- list("mod1" = mod1_std, "mod2" = mod2_std, "mod3" = mod3_std)
 modelplot(models, 
           coef_omit="Intercept", size=1.3) +
   ggtitle("Standardized coefficients") +
   geom_vline(xintercept = 0, size=1.5) +
+  scale_y_discrete(limits=rev) +
+  theme_bw(base_size = 16) +
+  theme(legend.position = "inside",
+        legend.position.inside = c(0.85, 0.2))
+
+#' or use modelsummary with `standardize = "refit"`
+
+modelplot(list("mod1" = mod1, "mod2" = mod2, "mod3" = mod3),
+          standardize = "refit",
+          coef_omit="Intercept", size=1.3) +
+  ggtitle("Standardized coefficients") +
+  geom_vline(xintercept = 0, linewidth=1.5) +
   scale_y_discrete(limits=rev) +
   theme_bw(base_size = 16) +
   theme(legend.position = "inside",
