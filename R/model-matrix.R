d13f2ba05d0108740f2c99715e32b3faf1f7aebc
Michael Friendly <friendly@yorku.ca>
2024-06-24 14:40:06 -0400

    more on formulas

--- R/model-matrix.R
+++ R/model-matrix.R
@@ -1,5 +1,19 @@
 # model matrix
 
+# expanding a formula
+
+f1 = formula(y ~ a * b * c * d * e)
+#To spell out the interaction terms, we extract the terms from the value returned by terms.formula():
+  
+terms = attr(terms.formula(f1), "term.labels")
+#And then we can convert it back to a formula:
+  
+f1_exp = as.formula(sprintf("y ~ %s", paste(terms, collapse="+")))
+
+f2 <- formula(y ~ (x1 + x2 + x3 + x4)^2)
+terms = attr(terms.formula(f2), "term.labels")
+f2_exp = as.formula(sprintf("y ~ %s", paste(terms, collapse="+"))) 
+
 set.seed(42)
 inc <- round(runif(n=9, 20, 40))
 type <- rep(c("bc", "wc", "prof"), each =3)
