b4c283118cd57ee2ee5d4e8f0e1e6bbdc8679d1d
Michael Friendly <friendly@yorku.ca>
2024-10-07 19:43:47 -0400

    laptop edits while away

---
+++ R/penguin/peng-bullseye.R
@@ -0,0 +1,11 @@
+# Bullseye plots -- https://cran.r-project.org/web/packages/bullseye/vignettes/vis_pairwise.html
+
+load(here::here("data", "peng.RData"))
+source("R/penguin/penguin-colors.R")
+
+library(bullseye)
+
+#The pairwise structure has multiple association scores when each (x,y) pair appears multiple times in the pairwise structure.
+
+scores <- pairwise_scores(peng, by="species")
+plot(scores) 
