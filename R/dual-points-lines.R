a03f0c41c56b78b76cf42616e47cac66cc912a14
Michael Friendly <friendly@yorku.ca>
2024-10-22 13:32:32 -0400

    add fig on duality of pts and lines in Ch07

--- R/dual-points-lines.R
+++ R/dual-points-lines.R
@@ -5,61 +5,69 @@ library(matlib)
 intersections <- function(A, b) {
   neq <- nrow(A)
   res <- matrix(NA, nrow=neq*(neq - 1)/2, ncol=2)
+  rnames <- rep("", neq*(neq - 1)/2)
   colnames(res) <- c("x", "y")
   k <- 0
   for (i in 1:(neq - 1)) {
     for (j in (i + 1):neq) {
+      rnames[k] <- paste0(i, ":", j)
       k <- k + 1
-      x <- try(solve(A[c(i, j), ], b[c(i, j)]), silent=TRUE)
-      if (!inherits(x, "try-error")) res[k, ] <- x
+      xy <- try(solve(A[c(i, j), ], b[c(i, j)]), silent=TRUE)
+      if (!inherits(x, "try-error")) res[k, ] <- xy
     }
   }
+#  rownames(res) <- rnames
   res
 }
 
-A <- matrix(c( 1, 2, 0,
-              -1, 2, 1), 3, 2) |>
-  print()
 
+A <- matrix(c(1, 1, 0,
+              -1, 1, 1), 3, 2) 
+b <- c(2, 1/2, 1)
+cbind(A, b)
 
-b <- c(2, 1, 1)
+showEqn(A, b, vars = c("x", "y"), simplify = TRUE)
 
-eqn <- showEqn(A, b, vars = c("x", "y"), simplify = TRUE)
+col <- c("red", "blue", "darkgreen")
+plotEqn(A, b, vars = c("x", "y"), col = col)
 
-plotEqn(A, b, vars = c("x", "y"))
-
-pts <- intersections(A,b) |> print()
+pts <- intersections(A, b) |> print()
 
 # doesn't work
-plotEqn(A, b, vars = c("x", "y"),
-        labels = c("y = x - 2",
-                   "y = 1/2 - x",
-                   "y = 1"))
+# plotEqn(A, b, vars = c("x", "y"),
+#         labels = c("y = x - 2",
+#                    "y = 1/2 - x",
+#                    "y = 1"))
 
-a <- c(-2, .5, 1)
-b <- c( 1, -1, 0)
+# Equations as intercepts & slopes in data space
+x <- c(-2, .5, 1)
+y <- c( 1, -1, 0)
+
 # plot lines in data space
-x <- a
-y <- b
 op <- par(mar = c(5, 5, 1, 1) + .1,
           mfrow = c(1, 2))
 plot(0,0, type ="n",
      xlab = "x",
      ylab = "y",
      xlim = c(-1, 4), ylim = c(-3, 2),
-     cex.lab = 2, asp = 1)
+     cex.lab = 2, asp = 1,
+     main = "Data space")
 abline(h = 0, v = 0, col = "gray")
-for (i in seq_along(a)) {
+for (i in seq_along(x)) {
   abline(x[i], y[i], col = col[i], lwd = 2)
 }
-#points(pts, pch = 16)
+
+# cheating
+pts <- matrix(c(1.25, 3, -.5,
+                -.75, 1,   1), 3, 2)
+points(pts, pch = 16)
 
 # simplify equations
 eqn <- c("y = x - 2", 
          "y = 0.5 - x",
          "y = 1")
 angle <- c(45, -45, 0)
-xl <- c(-0.5, 2,  1)
+xl <- c(-0.5, 2,  1.2)
 yl <- c(-2.0, -1.2, 1.)
 pos <- c(4, 4, 3)
 for (i in 1:3) {
@@ -74,17 +82,23 @@ for (i in 1:3) {
 # plot points in beta space
 op <- par(mar = c(5, 5, 1, 1) + .1)
 col <- c("red", "blue", "darkgreen")
-pch <- c(16, 17, 19)
+pch <- 16
+a <- x
+b <- y
 plot(a, b, col = col, 
      pch = pch, cex = 2, cex.lab = 2,
      xlim = c(-3, 2), ylim = c(-2, 2),
      xlab = expression(beta[0]),
      ylab = expression(beta[1]),
-     asp = 1)
-text(a, b, label = paste0("(", a, ",", b, ")"), col = col, pos = 1)
+     asp = 1,
+     main = "Beta space")
+abline(h = 0, v = 0, col = "gray")
+lines(rep(a, length.out =4), rep(b, length.out =4), col = "black")
+text(a, b, label = paste0("(", a, ",", b, ")"), col = col, pos = 1, offset = .75)
+text(a, b, label = eqn, col = col, pos = 3, offset = .75)
 par(op)
 
-
+#-------------------------------------------
 # try with workers data 
 data(workers, package = "matlib")   
 head(workers)
