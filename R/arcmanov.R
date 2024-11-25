3ec4b9495109101dc5f6a5450ea34bda68253962
Michael Friendly <friendly@yorku.ca>
2024-11-24 20:43:16 -0500

    arcmanov.R

--- R/arcmanov.R
+++ R/arcmanov.R
@@ -14,10 +14,30 @@ means <- tibble::tribble(
 )
 ng <- nrow(means)
 
-means$r <- round(runif(ng, min =0.4, max =0.9), 0.1)
+set.seed(47)
+means$s1 <- round(runif(ng, min =1.5, max = 4), digits=2)
+means$s2 <- round(runif(ng, min =1.5, max = 4), digits=2)
+means$r <- round(runif(ng, min =0.4, max =0.9), digits=2)
 
-means <- means |>
-  mutate(r = round(runif(1, min =0.4, max =0.9), 0.1),
-         sd1 = runif(1.5, 2.5))
+make_mu_sigma <- function(group, m1, m2, s1, s2, r, names = c("y1", "y2")) {
+  if (missing(group)) group <- 1
+  mu <- c(m1, m2)
+  names(mu) <- names
+  list(
+    group = group,
+    mu = mu,
+    Sigma = matrix(c(s1^2, r*s1*s2,
+                      r*s1*s2, s2^2),
+                      nrow = 2, ncol = 2,
+                      dimnames = list(names, names))
+  )
+}
 
+make_mu_sigma(1, 10, 15, 1.5, 2, .5)
 
+Grps <- list()
+for (i in 1:ng) {
+  Grps[[i]] <- make_mu_sigma(means$group[i], 
+                             means$y1[i], means$y2[i],
+                             means$s1[i], means$s2[i], means$r[i])
+}
