ba617a9631cb55e8809f47cb83a0a3cb7521c63f
Michael Friendly <friendly@yorku.ca>
2025-04-18 17:11:26 -0400

    iris.qmd & iris-lda.R

--- R/iris/iris-lda.R
+++ R/iris/iris-lda.R
@@ -2,15 +2,43 @@
 # from: https://rpubs.com/Nolan/298913
 
 library(MASS)
-library(klaR)
+library(klaR) # partimat
 library(car)
 
 iris.lda <- lda(Species ~ ., iris)
+iris.lda
+names(iris.lda)
+
 str(iris.lda)
 
-plot(iris.lda, col = as.integer(iris$Species))
+plot(iris.lda, col = as.integer(iris$Species), abbrev = 3)
 
-dataEllipse(LD2 ~ LD1, data=iris.lda, groups = iris$Species, levels = 0.68 )
+iris.scores <- data.frame(
+  Species = iris$Species,
+  predict(iris.lda)$x
+)
+str(iris.scores)
+
+col <- scales::hue_pal()(3)
+dataEllipse(LD2 ~ LD1 | Species, data=iris.scores, 
+            levels = 0.68, 
+            fill = TRUE, fill.alpha = 0.05,
+            col = col,
+            pch = 15:17,
+            grid = FALSE,
+            label.pos = "top",
+            label.cex = 1.8)
+
+# point id
+dataEllipse(LD2 ~ LD1 | Species, data=iris.scores, 
+            levels = 0.68, 
+            fill = TRUE, fill.alpha = 0.05,
+            col = col,
+            pch = 15:17,
+            grid = FALSE,
+            label.cex = 1.5,
+            id = list(n = 2, col = col))
+
 
 partimat(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, 
          data=iris, method="lda")
