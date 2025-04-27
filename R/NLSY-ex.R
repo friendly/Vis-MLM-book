426bbc1dca5bf16686af6d927d261c66600b0a60
Michael Friendly <friendly@yorku.ca>
2025-04-27 16:33:19 -0400

    more on Ch10 NLSY

--- R/NLSY-ex.R
+++ R/NLSY-ex.R
@@ -15,7 +15,8 @@ set.seed(47)
 ggplot(NLSY, aes(x = read, y = math)) +
   geom_jitter()+
   geom_smooth(method = lm, formula = y~x, fill = "blue", alpha = 0.2) +
-  geom_smooth(method = loess, se = FALSE, color = "red", linewidth = 2)
+  geom_smooth(method = loess, se = FALSE, color = "red", linewidth = 2) +
+  geom_rug()
 
 mod0 <- lm(math ~ read, data=NLSY)
 infl <- influencePlot(mod0)
@@ -40,55 +41,97 @@ ggplot(NLSY_long, aes(x=value, fill=variable)) +
   theme(legend.position = "none") 
 
 
-# test control variables by themselves
-# -------------------------------------
-mod1 <- lm(cbind(read, math) ~ log2(income) + educ, 
+mod0 <- lm(cbind(read, math) ~ log2(income) + educ + antisoc + hyperact, 
            data= NLSY |> filter(income != 0))
 
-anova(mod1)  # Type I, sequential test
-Anova(mod1)  # Type II
 
-coef(mod1)
-coefplot(mod1, fill = TRUE,
-         col = c("green", "brown"),
-         ylim = c(-0.5, 2.5))
+# test control variables by themselves
+# -------------------------------------
+NLSY.mod1 <- lm(cbind(read, math) ~ log2(income) + educ, 
+           data= NLSY |> filter(income != 0))
 
-heplot(mod1, fill=TRUE, fill.alpha = 0.2)
+anova(NLSY.mod1)  # Type I, sequential test
+Anova(NLSY.mod1)  # Type II
+
+coef(NLSY.mod1)
+tidy(NLSY.mod1)
+
+coefplot(NLSY.mod1, fill = TRUE,
+         col = c("darkgreen", "brown"),
+         lwd = 2,
+         ylim = c(-0.5, 3),
+         main = "Bivariate coefficient plot for reading and math\nwith 95% confidence ellipses")
+
+cqplot(NLSY.mod1, id.n = 5)
+
+heplot(NLSY.mod1, fill=TRUE, fill.alpha = 0.2)
 
 # test of overall regression
-coefs <- rownames(coef(mod1))[-1]
-linearHypothesis(mod1, coefs)
+coefs <- rownames(coef(NLSY.mod1))[-1]
+linearHypothesis(NLSY.mod1, coefs) |> print(SSP = FALSE)
 
-heplot(mod1, fill=TRUE, hypotheses = list("Overall" = coefs))
+heplot(NLSY.mod1, fill=TRUE, hypotheses = list("Overall" = coefs))
 
 
 # additional contribution of antisoc + hyperact over income + educ
 # ----------------------------------------------------------------
-mod2 <- lm(cbind(read,math) ~ antisoc + hyperact + log2(income) + educ, 
+NLSY.mod2 <- lm(cbind(read,math) ~ antisoc + hyperact + log2(income) + educ, 
            data = NLSY |> filter(income != 0))
 # use update
-update(mod1, . ~ . + antisoc + hyperact)
+update(NLSY.mod1, . ~ . + antisoc + hyperact)
 
-Anova(mod2)
+Anova(NLSY.mod2)
 
-coefs <- rownames(coef(mod2))[-1]
-heplot(mod2, fill=TRUE, 
-       hypotheses=list("Overall"=coefs, "mod2|mod1"=coefs[1:2]))
+coefs <- rownames(coef(NLSY.mod2))[-1]
+heplot(NLSY.mod2, fill=TRUE, 
+       hypotheses=list("Overall"=coefs, "NLSY.mod2|NLSY.mod1"=coefs[1:2]))
 
-linearHypothesis(mod2, coefs[1:2], title = "mod2 | mod1")
+linearHypothesis(NLSY.mod2, coefs[1:2], title = "NLSY.mod2 | NLSY.mod1")
 
-linearHypothesis(mod2, coefs, title = "Overall")
+linearHypothesis(NLSY.mod2, coefs, title = "Overall")
 
-heplot(mod2, fill=TRUE, hypotheses=list("mod2|mod1"=coefs[1:2]))
+heplot(NLSY.mod2, fill=TRUE, hypotheses=list("NLSY.mod2|NLSY.mod1"=coefs[1:2]))
 
-coef(mod2)
-coefplot(mod2, fill = TRUE)
+coef(NLSY.mod2)
+coefplot(NLSY.mod2, fill = TRUE)
+
+idx < cqplot(NLSY.mod2, id.n = 5)
+
+#-- robust methods
+
+NLSY.rlm <-  heplots::robmlm(cbind(read,math) ~ antisoc + hyperact + log2(income) + educ, 
+                data = NLSY |> filter(income != 0))
+Anova(NLSY.rlm)
+
+wts <- NLSY.rlm$weights
+small <- wts < 0.1
+sum(small)
+ymax <- 1
+plot(wts,
+     ylim = c(-0.1, ymax),
+     ylab = "Robust observation weight")
+index <- seq_along(wts)
+segments(index[small], wts[small], index[small], 1)
+text(index[small], wts[small], 
+     label= index[small], 
+     pos = rep(c(1,3), length = length(index[small])), 
+     xpd = TRUE)
+
+
+# doesn't handle observation weights-- fix Mahalanobis?
+cqplot(NLSY.rlm, id.n = 5)
+
+
+
 
 # Is there an update method for the LHS of an mlm?
 
-update(mod2, read ~ .)
-update(mod2, math ~ .)
+update(NLSY.mod2, read ~ .)
+update(NLSY.mod2, math ~ .)
 
-update(mod1, cbind(read, math, antisoc, hyperact) ~ .)
+update(NLSY.mod1, cbind(read, math, antisoc, hyperact) ~ .)
 
+
+library(equatiomatic)
+extract_eq(NLSY.mod1)
 
--- working-text/roy-bargmann.md
+++ working-text/roy-bargmann.md
@@ -1,4 +1,5 @@
 ## Stepdown analysis of multivariate models in R
+# https://stackoverflow.com/questions/79594493/stepdown-analysis-of-multivariate-models-in-r
 
 Roy - Bargmann stepdown tests are commonly recommended for multivariate linear models fit with `lm()` as:
 
@@ -20,3 +21,70 @@ Sets of models like these can be fit `manuall` as above, but I'll looking for so
 in a function and return these in a list. 
 
 I wonder if `update()` can help here or else some other way to manipulate the model formula.
+
+Tests:
+
+mod1 <- lm(cbind(read, math) ~ log2(income) + educ, 
+           data= heplots::NLSY |> filter(income != 0))
+mod2 <- lm(cbind(read,math) ~ antisoc + hyperact + log2(income) + educ, 
+           data = heplots::NLSY |> filter(income != 0))
+
+# extract univariate models
+update(mod2, read ~ .) |> as.formula()
+update(mod2, math ~ .)
+
+
+update(mod1, cbind(read, math, antisoc, hyperact) ~ .)
+
+
+# Ben Bolker
+
+np <- 3; nr <- 5
+resp_vars <- paste0("y", 1:np)
+pred_vars <- paste0("x", 1:nr)
+ffun <- function(i) {
+   pp <- sprintf("cbind(%s)", paste(resp_vars[i:np], collapse = ", "))
+   rr <- c(pred_vars, resp_vars[seq_len(i-1)])
+   ff <- reformulate(rr, resp = pp)
+}
+(form_list <- lapply(1:np, ffun))
+
+
+# MLM model equations
+
+library(equatiomatic)
+library(matlib)
+
+mod_eq <- lm(cbind(read, math) ~ income + educ, 
+           data= heplots::NLSY)
+
+extract_eq(mod_eq)
+
+$$
+\operatorname{cbind(read,\ math)} = \alpha + \beta_{1}(\operatorname{income}) + \beta_{2}(\operatorname{educ}) + 
+                                    \alpha + \beta_{4}(\operatorname{income}) + \beta_{5}(\operatorname{educ}) + \epsilon
+$$
+
+Should be:
+
+(\mathbf{y}_\textrm{read}, \mathbf{y}_\textrm{math}) =
+$$
+\operatorname{(read,\ math)} = \boldsymbol{\alpha} + 
+                               \boldsymbol{\beta}_{1}(\operatorname{income}) + 
+                               \boldsymbol{\beta}_{2}(\operatorname{educ}) + 
+                               \boldsymbol{\epsilon}
+$$
+
+## Stepdown procedure
+
+from Tabachnick/Fidell:
+
+ANCOVA is used after MANOVA (or MANCOVA) in Roy– Bargmann stepdown analysis
+where the goal is to assess the contributions of the various DVs to a significant effect. One asks
+whether, after adjusting for differences on higher- priority DVs serving as covariates, there is any
+significant mean difference among groups on a lower- priority DV. That is, does a lower- priority
+DV provide additional separation of groups beyond that of the DVs already used? In this sense,
+ANCOVA is used as a tool in interpreting MANOVA results.
+
+
+
