d7eabfd1201144bc1889d2b4ccb79169cb2f00e4
Michael Friendly <friendly@yorku.ca>
2025-04-30 13:56:57 -0400

    Ch05 contrasts

--- R/contrasts.R
+++ R/contrasts.R
@@ -2,6 +2,35 @@
 
 library(matlib)
 
+levels <- letters[1:4]
+C <- contr.treatment(levels)
+C
+
+
+C <- contr.sum(levels) |> print()
+mu <- matrix(paste0("\\mu_", levels), 4, 1)
+
+X <- cbind(J(4,1), C)
+Eqn(latexMatrix(X),  "\\times", latexMatrix(mu), " = ",
+    latexMatrix(X) %*% latexMatrix(mu))
+
+
+# basic example
+
+X <- expand.grid(Treat = LETTERS[1:3],
+                 Sex = c("F", "M"))
+y <- outer(c(10, 20, 30), c(-5, 5), "+") |>
+  c() + round(rnorm(n=6, sd = 2), 2)
+df <- cbind(y, X)
+
+# dummy variables = contr.treatment
+model.matrix(y ~ Treat + Sex, data = df)
+
+# sum-to-zero
+contrasts(df$Treat) <- contr.sum(3)
+contrasts(df$Sex) <- contr.sum(2)
+model.matrix(y ~ Treat + Sex, data = df)
+
 ## Polynomial contrasts
 
 M <- outer(1:8, 0:3, `^`)
@@ -23,8 +52,7 @@ matplot(GramSchmidt(M),
 
 
 # contrasts
-library(matlib)
-c1 <- c(1,1,-1,-1)
+c1 <- c(1, 1, -1,-1)
 c2 <- c(1, -1, 0, 0)
 c3 <- c(0, 0, 1, -1)
 latexMatrix(matrix(c1, nrow=1))
@@ -37,12 +65,13 @@ t(C) %*% C
 
 # Factorial from kronecker product
 
-C.group <- contr.Sum(3) |> print()
-C.sex <- contr.Treatment(2) |> print()
+C.group <- contr.sum(3) |> print()
+C.sex <- contr.treatment(2) |> print()
 
 X.group <- cbind(1, C.group)
 rownames(X.group) <- paste0("A", 1:3)
-colnames(X.group) <- c("", "G12", "G13")
+colnames(X.group) <- c("", "G13", "G23")
+X.group
 
 X.sex   <- cbind(1, C.sex)
 rownames(X.sex) <- c("F", "M")
@@ -51,6 +80,9 @@ colnames(X.sex) <- c("", "MvF")
 #kronecker(X.group, X.sex)
 
 kronecker(X.sex, X.group, make.dimnames = TRUE) 
+
+kronecker(X.group, X.sex, make.dimnames = TRUE) 
+
 
 
 
