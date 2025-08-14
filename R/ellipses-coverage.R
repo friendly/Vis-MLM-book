84354c75da2d46b57828284ee625bb28d9a216a1
Michael Friendly <friendly@yorku.ca>
2025-08-14 13:54:31 -0400

    editing Ch11 re MT comments

--- R/ellipses-coverage.R
+++ R/ellipses-coverage.R
@@ -20,11 +20,11 @@ c <- qchisq(levels, df = 2) |> round(2) |> print()
 # labels <- expression(paste0("c =", chi^2, "(", levels, ") ==", c))
 # labels <- bquote(paste("c =", chi^2, "(", .(levels), ") =", .(c)))
 
-# lab <- c(
-#   bquote(paste("c =", chi^2, "(", .(levels[1]), ") =", .(c[1]))),
-#   bquote(paste("c =", chi^2, "(", .(levels[2]), ") =", .(c[2]))),
-#   bquote(paste("c =", chi^2, "(", .(levels[3]), ") =", .(c[3])))
-# )
+lab <- c(
+  bquote(paste("c =", chi^2, "(", .(levels[1]), ") =", .(c[1]))),
+  bquote(paste("c =", chi^2, "(", .(levels[2]), ") =", .(c[2]))),
+  bquote(paste("c =", chi^2, "(", .(levels[3]), ") =", .(c[3])))
+)
 
 lab1 <- bquote(paste("c =", chi[2]^2, "(", .(levels[1]), ") =", .(c[1])))
 lab2 <- bquote(paste("c =", chi[2]^2, "(", .(levels[2]), ") =", .(c[2])))
