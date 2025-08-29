8c02a5bba65e6f3eb6578220bc88e248a77386cd
Michael Friendly <friendly@yorku.ca>
2025-08-29 16:48:14 -0400

    MockJury example in Ch 11

--- R/MockJury-ex.R
+++ R/MockJury-ex.R
@@ -4,14 +4,20 @@
 
 library(car)
 library(heplots)
+library(candisc)
 library(ggpubr)
 library(patchwork)
 
 data(MockJury, package = "heplots")
-str(MockJury)
+names(MockJury)
 
 table(MockJury$Attr)
 table(MockJury$Attr, MockJury$Crime)
+
+# consider contrasts?
+levels(MockJury$Attr)
+C <- matrix(c(1, -0.5, -0.5,
+              0,  1,    -1), nrow=3)
 
 jury.mod <- lm( cbind(Serious, Years) ~ Attr * Crime, data=MockJury)
 Anova(jury.mod, test = "Roy")
@@ -51,4 +57,56 @@ p2 <- ggline(MockJury,
   legend_inside(c(.75, .9))
 
 p1 + p2
+
+# Manipulation check
+
+jury.mod1 <- lm( cbind(phyattr, exciting, sociable, happy, independent) ~ Attr * Crime, 
+                  data=MockJury)
+Anova(jury.mod1)
+uniStats(jury.mod1)
+
+op <- par(mar = c(4, 4, 1, 1)+0.5, mfrow = c(1, 2))
+heplot(jury.mod1, 
+       terms = "Attr", factor.means = "Attr",
+       fill = TRUE, fill.alpha = 0.2,
+       cex.lab = 1.6, asp = 1)
+
+heplot(jury.mod1,
+       variables = c(1,5),
+       terms = "Attr", factor.means = "Attr",
+       fill = TRUE, fill.alpha = 0.2,
+       cex.lab = 1.6, asp = 1)
+par(op)
+
+
+pairs(jury.mod1, 
+      terms = "Attr", factor.means = "Attr",
+      fill = TRUE, fill.alpha = 0.1)
+
+jury.can1 <- candisc(jury.mod1, term = "Attr") |>
+  print()
+
+op <- par(mar = c(4, 4, 1, 1)+0.5)
+plot(jury.can1, rev.axes = c(TRUE, TRUE),
+     lwd = 3,
+     var.lwd = 2,
+     var.cex = 1.4,
+     var.col = "black",
+     pch = 15:17,
+     cex = 1.2,
+     cex.lab = 1.5)
+par(op)
+
+jury.mod2 <- lm(cbind(exciting, calm, independent, sincere, warm, phyattr, sociable, 
+                      kind, intelligent, strong, sophisticated, happy, ownPA) ~ Attr * Crime,
+                data = MockJury)
+uniStats(jury.mod2)
+
+jury.can2 <- candisc(jury.mod2, term = "Attr")
+  
+plot(jury.can2, rev.axes = c(FALSE, TRUE),
+     lwd = 3,
+     var.lwd = 2,
+     var.col = "black",
+     pch = 15:17)
 
