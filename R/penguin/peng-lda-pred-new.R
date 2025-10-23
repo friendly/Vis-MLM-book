7e8b363c96b23b4f528d0996eda137de85a1bf9b
Michael Friendly <friendly@yorku.ca>
2025-10-23 17:44:53 -0400

    R/predict_discrim.R and work on 10-discrim.qmd

--- R/penguin/peng-lda-pred-new.R
+++ R/penguin/peng-lda-pred-new.R
@@ -8,35 +8,25 @@ source("R/penguin/penguin-colors.R")
 peng.lda <- lda(species ~  bill_length + bill_depth + flipper_length + body_mass, data = peng)
 peng.lda
 
-
-# peng_new <- tibble(
-#   id = c("Abe", "Betsy", "Chloe"),
-#   species = rep(NA, 3),
-#   island = rep("Z", 3),
-#   bill_length = c(35, 52, 52),
-#   bill_depth= c(18, 20, 15),
-#   flipper_length = c(220, 190, 210),
-#   body_mass = c(5000, 3900, 4000),
-#   sex = c("m", "f", "f")
-# )
-
-# use rownames
 peng_new <- data.frame(
-  species = rep(NA, 3),
-  island = rep("Z", 3),
-  bill_length = c(35, 52, 52),
-  bill_depth= c(18, 20, 15),
-  flipper_length = c(220, 190, 210),
-  body_mass = c(5000, 3900, 4000),
-  sex = c("m", "f", "f")
+  species = rep(NA, 4),
+  island = rep("Z", 4),
+  bill_length = c(35, 52, 52, 50),
+  bill_depth= c(18, 20, 15, 16),
+  flipper_length = c(220, 190, 210, 190),
+  body_mass = c(5000, 3900, 4000, 3800),
+  sex = c("m", "f", "f", "m")
 )
 
-rownames(peng_new) <- c("Abe", "Betsy", "Chloe")
+# use rownames
+rownames(peng_new) <- c("Abe", "Betsy", "Chloe", "Dave")
 peng_new
 
 
 
-predict(peng.lda, newdata = new_peng)
+peng_pred <- predict(peng.lda, newdata = peng_new)
+
+zapsmall(peng_pred$posterior)
 
 pred_lda <- function(object, newdata, ...) {
   if (missing(newdata)) {
@@ -61,4 +51,9 @@ pred_lda <- function(object, newdata, ...) {
 
 pred_lda(peng.lda, newdata = peng_new[, -1])
 
-#tidy_pred(pend.lda, "species")
+# try predict_discrim
+# 
+source("R/predict_discrim.R")
+
+predict_discrim(peng.lda, newdata = peng_new[, -1])
+
---
+++ R/predict_discrim.R
@@ -0,0 +1,55 @@
+#' Predicted values for discriminant analysis
+#' 
+#' \code{predict_discrim} calculates predicted class membership values for a linear or quadratic discriminant analysis,
+#' returning a `data.frame` suitable for graphing or other analysis.
+#' 
+#' @details
+#' The `predict()` methods provided for [MASS::lda()] and [MASS::qda()] are a mess, because they return their results as
+#' a list, with components `class`, `posterior` and `x`
+#' 
+#' 
+#' @param object   An object of class `"lda"` or `"qda"`  such as results from [MASS::lda()] or [MASS::qda()] 
+#' @param newdata  A data frame of cases to be classified or, if `object` has a formula, a data frame with columns of the same names as the variables used. A vector will be interpreted as a row vector. If `newdata` is missing, an attempt will be made to retrieve the data used to fit the `lda` object.
+#' @param prior The prior probabilities of the classes. By default, taken to be the proportions in what was set in the call to [MASS::lda()] or [MASS::qda()] 
+#' @param dimen The dimension of the space to be used. If this is less than min(p, ng-1), only the first `dimen` discriminant components are used 
+#' @param scores A logical. If `TRUE`, the discriminant scores of the cases in `newdata` are appended as additional columns in the the result, with names `LD1`, `LD2`, ...
+#' @param ...      arguments based from or to other methods, not yet used here
+#' @md
+#' @returns        A data.frame, containing the values of the `newdata` variables, the predicted class of the observations and the maximum value of the posterior probabilities of the classes. `rownames()` in the result are inherited from those in `newdata`.
+#' @export
+#' @examples
+#' # none yet.
+predict_discrim <- function(object, 
+                            newdata, 
+                            prior = object$prior,
+                            dimen,
+                            scores = FALSE,
+                            ...) {
+  cls <- class(object)
+  if (!cls %in% c("lda", "qda")) {
+    stop(paste('object must be of class "lda" or "qda", not', cls))
+  }
+  if (missing(newdata)) {
+    newdata <- insight::get_modelmatrix(object) |>
+      as.data.frame() |>
+      dplyr::select(-"(Intercept)") 
+  }
+  nv <- ncol(newdata)
+  pred <- predict(object, newdata, prior=prior, type = "prob")
+  class <- pred$class
+  probs <- pred$posterior
+  maxp <- apply(probs, 1, max)
+
+    # get response variable name to substitute for `class`
+  response <- insight::find_response(object)
+  
+  ret <- cbind(newdata, class, maxp)
+  colnames(ret)[nv+1] <- response
+  
+  if (scores) {
+    scores <- pred$x
+    ret <- cbind(ret, scores)
+  }
+  
+  ret
+}
